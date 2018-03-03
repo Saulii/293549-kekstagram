ca6a6af8c25c467b258298d92e4bc16f28816e6b
Saulii <lidiya313@gmail.com>
2018-03-03 16:41:48 +0100

    внесла изменения в функцию

--- js/pictures.js
+++ js/pictures.js
@@ -7,13 +7,13 @@ var PICTURES_QUANTITY = 25;
 var arrPicture = [];
 
 //функция, генерирующая случайные числа из заданного диапазона
-var getRandomNumberFromRange = function (min, max) {
+var getRandomNumberFromRange = function(min, max) {
   var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
   return randomNumber;
 }
-  //объявляю массив строк для генерации рандомных комментариев 
+//объявляю массив строк для генерации рандомных комментариев 
 var mockComments = [
-  'Всё отлично!', 'В целом всё неплохо. Но не всё.', 
+  'Всё отлично!', 'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
@@ -27,60 +27,61 @@ var getArrayWithComments = function() {
   var randomCommentsNumber = getRandomNumberFromRange(1, 2); //Для комментария нужно взять одно или два случайных предложений из предложенных ниже//
   var randomComments = [];
   for (var i = 1; i <= randomCommentsNumber; i++) {
-  	var randomCommentIndex = getRandomNumberFromRange(0, mockCommentsLength - 1);
+    var randomCommentIndex = getRandomNumberFromRange(0, mockCommentsLength - 1);
     var randomComment = mockComments[randomCommentIndex];
-  	randomComments.push(randomComment);
+    randomComments.push(randomComment);
   }
   return randomComments;
 }
 // добавляем объекты в массив
-for (var i = 1; i <= PICTURES_QUANTITY; i++){
-	var randomLikesNumber = getRandomNumberFromRange(LIKES_MIN, LIKES_MAX);
-	var randomComments = getArrayWithComments();
-	var photoObject = {
-  	url: 'photos/' + i + '.jpg',
+for (var i = 1; i <= PICTURES_QUANTITY; i++) {
+  var randomLikesNumber = getRandomNumberFromRange(LIKES_MIN, LIKES_MAX);
+  var randomComments = getArrayWithComments();
+  var photoObject = {
+    url: 'photos/' + i + '.jpg',
     likes: randomLikesNumber,
     comments: randomComments
   };
-  arrPicture.push(photoObject); 
+  arrPicture.push(photoObject);
 }
 
 var picturesContainer = document.querySelector('.pictures');
-//показываем элемент на сайте//
-var galleryElement = document.querySelector('.gallery-overlay');
-galleryElement.classList.remove('hidden');
 var imageLinkTemplate = document.querySelector('#picture-template').content;
-//функция, клонируем элементы 
-var getPictureElements = function(){
-  var imageLinkElement = imageLinkTemplate.cloneNode(true);
-  var imgElement = imageLinkElement.querySelector('img');
-  imgElement.setAttribute('src', arrPicture[i].url);
-  var likeElement = imageLinkElement.querySelector('.picture-likes');
-  likeElement.textContent = arrPicture[i].likes;
-  var commentElement = imageLinkElement.querySelector('.picture-comments');
-  commentElement.textContent = arrPicture[i].comments.length;
-  var imageGalleryElem = imageLinkElement.querySelector('img');
-  imageGalleryElem.textContent = arrPicture[i].url;
-  return imageLinkElement
+//функция, заполняем данными элементы картинки 
+var setPictureInfoFromMockData = function(pictureParts, mockData) {
+  var imgElement = pictureParts.img;
+  var likeElement = pictureParts.like;
+  var commentElement = pictureParts.comment;
+  imgElement.setAttribute('src', mockData.url);
+  likeElement.textContent = mockData.likes;
+  commentElement.textContent = mockData.comments.length;
 }
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < arrPicture.length; i++) {
-  var pictureObj = arrPicture[i];
-  fragment.appendChild(getPictureElements(pictureObj));
+  var imageLinkElement = imageLinkTemplate.cloneNode(true);
+  var imagePictureParts = {
+    img: imageLinkElement.querySelector('img'),
+    like: imageLinkElement.querySelector('.picture-likes'),
+    comment: imageLinkElement.querySelector('.picture-comments')
+  };
+  var pictureData = arrPicture[i];
+  setPictureInfoFromMockData(imagePictureParts, pictureData)
+  fragment.appendChild(imageLinkElement);
 }
 picturesContainer.appendChild(fragment);
 
+var firstDataElement = arrPicture[0];
+var galleryElement = document.querySelector('.gallery-overlay');
+var galleryPictureParts = {
+  img: galleryElement.querySelector('.gallery-overlay-image'),
+  like: galleryElement.querySelector('.likes-count'),
+  comment: galleryElement.querySelector('.comments-count')
+};
+setPictureInfoFromMockData(galleryPictureParts, firstDataElement);
+//показываем элемент на сайте
+
+galleryElement.classList.remove('hidden');
   
-          
-
-
-
-
-
-
-
-
-
 
 
 
