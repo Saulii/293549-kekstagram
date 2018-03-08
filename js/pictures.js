8f6eb9cbfc10ad653042bd285478f84598efe2fc
Saulii <lidiya313@gmail.com>
2018-03-08 13:23:47 +0100

    изменения, валидация кода .js

--- js/pictures.js
+++ js/pictures.js
@@ -1,17 +1,17 @@
-// объявляю константы 
+//объявляю константы// 
 var LIKES_MIN = 15;
 var LIKES_MAX = 250;
 var PICTURES_QUANTITY = 25;
 
-//объявляю массив, чтобы потом генерировать в него созданые объекты
+//объявляю массив, чтобы потом генерировать в него созданые объекты//
 var arrPicture = [];
 
-//функция, генерирующая случайные числа из заданного диапазона
-var getRandomNumberFromRange = function(min, max) {
+//функция, генерирующая случайные числа из заданного диапазона//
+var getRandomNumberFromRange = function (min, max) {
   var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
   return randomNumber;
 };
-//объявляю массив строк для генерации рандомных комментариев 
+//объявляю массив строк для генерации рандомных комментариев//
 var mockComments = [
   'Всё отлично!', 'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -19,11 +19,9 @@ var mockComments = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-
 var mockCommentsLength = mockComments.length;
 var getArrayWithComments = function() {
-
-  var randomCommentsNumber = getRandomNumberFromRange(1, 2); //Для комментария нужно взять одно или два случайных предложений из предложенных ниже//
+  var randomCommentsNumber = getRandomNumberFromRange (1, 2); //Для комментария нужно взять одно или два случайных предложений из предложенных ниже//
   var randomComments = [];
   for (var i = 1; i <= randomCommentsNumber; i++) {
     var randomCommentIndex = getRandomNumberFromRange(0, mockCommentsLength - 1);
@@ -32,7 +30,7 @@ var getArrayWithComments = function() {
   }
   return randomComments;
 };
-// добавляем объекты в массив
+// добавляем объекты в массив//
 for (var i = 1; i <= PICTURES_QUANTITY; i++) {
   var randomLikesNumber = getRandomNumberFromRange(LIKES_MIN, LIKES_MAX);
   var randomComments = getArrayWithComments();
@@ -46,8 +44,8 @@ for (var i = 1; i <= PICTURES_QUANTITY; i++) {
 
 var picturesContainer = document.querySelector('.pictures');
 var imageLinkTemplate = document.querySelector('#picture-template').content;
-//функция, заполняем данными элементы картинки 
-var setPictureInfoFromMockData = function(pictureParts, mockData) {
+//функция, заполняем данными элементы картинки//
+var setPictureInfoFromMockData = function (pictureParts, mockData) {
   var imgElement = pictureParts.img;
   var likeElement = pictureParts.like;
   var commentElement = pictureParts.comment;
@@ -77,7 +75,7 @@ var galleryPictureParts = {
   comment: galleryElement.querySelector('.comments-count')
 };
 setPictureInfoFromMockData(galleryPictureParts, firstDataElement);
-//показываем элемент на сайте
+//показываем элемент на сайте//
 
 galleryElement.classList.remove('hidden');
 
